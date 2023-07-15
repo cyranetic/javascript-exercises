20aa91c5e1a55d33052f1085452b397f182fd361
Sara Segel <sara.segel@gmail.com>
2023-07-16 00:38:52 +0900

    pass all tests for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  //there are no string reverse methods, so set up array to hold characters we want to reverse using reverse array method
+  let stringArray = [];
+  //add each character of string to array
+  for (let i = 0; i < string.length; i++) {
+    stringArray.push(string.charAt(i));
+  }
+  //reverse the array
+  stringArray = stringArray.reverse();
+  //concatenate array elements into a string
+  let reversedString = stringArray.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
